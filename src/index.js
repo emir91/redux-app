7a70d280dc0af87b2fcf1fc712d1d7c3a9f3a006
emir91 <vranacemir@gmail.com>
2021-05-04 21:09:50 +0200

    Add redux to the project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
+
+render(
+  <Provider store={createStore()}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
