07af894a8dbcf5a93521b88b70dbccd9077f5ba1
emir91 <vranacemir@gmail.com>
2021-05-05 12:42:23 +0200

    Connect SongList with store, fetch data from store and display it

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -4,9 +4,25 @@ import { connect } from 'react-redux';
 class SongList extends Component {
     render() {
         return (
-            <div>SongList</div>
+            <div>
+                <h2>SongList</h2>
+                {this.props.songs.map(song => {
+                    return(
+                        <div className="item" key={song.title}>
+                            <div className="left">{song.title}</div>
+                            <div className="right">
+                                <button className="primary">Select</button>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         )
     }
 }
 
-export default connect()(SongList);
+const mapStateToProps = state => {
+    return {songs: state.songs};
+}
+
+export default connect(mapStateToProps)(SongList);
