d24c78c3867fcf2f9615594d98dad6487fc8da58
emir91 <vranacemir@gmail.com>
2021-05-05 14:21:03 +0200

    Create SongDetail component

---
+++ src/components/SongDetail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const SongDetail = () => {
+    return (
+        <div>
+            <h3>Details for:</h3>
+            <p>Title: ________</p>
+            <p>Duration: ________</p>
+        </div>
+    );
+}
+
+export default SongDetail;
